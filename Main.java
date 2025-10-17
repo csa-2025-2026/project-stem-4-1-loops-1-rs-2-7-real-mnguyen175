9568bddd040df64635058c09855d645f7cf42d4d
mnguyen175 <maryannguyen2017@gmail.com>
2025-10-17 18:16:24 +0000

    count

--- Main.java
+++ Main.java
@@ -5,5 +5,14 @@ public class Main
   public static void main(String[] args)
   {
     // write solutions to problems here
+
+    // write a loop that prints the number between 3 and 25 
+
+    int count = 3; 
+    while (count <= 25) 
+    {
+      System.out.println(count);
+      count++; 
+    }
   }
 }
