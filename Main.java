1a7c9a88d7b55e3e0118ddeb3525cc2f88e05d3f
mnguyen175 <maryannguyen2017@gmail.com>
2025-10-20 18:22:10 +0000

    yes

--- Main.java
+++ Main.java
@@ -14,5 +14,58 @@ public class Main
       System.out.println(count);
       count += 2; 
     }
+
+    // Problem 1
+    Scanner sc = new Scanner(System.in);
+    System.out.println("Enter positive numbers to add. Enter -1 to stop"); 
+    int sum = 0; 
+    int userInput = 0; 
+
+    while (userInput != -1)
+    {
+      userInput = sc.nextInt();
+      sum += userInput;
+    }
+    sum++; // note that when the user enters -1, we add -1 to the sum 
+           // so we ++ to add back the 1 
+    System.out.println("The sum is " + sum);
+
+    // Problem 2 
+    int N; 
+    System.out.println("How many numbers do you want to enter?"); 
+    N = sc.nextInt();
+    int max = Integer.MIN_VALUE; 
+
+    int counter = 0;
+    while (counter < N)
+    {
+      userInput = sc.nextInt();
+      if (userInput > max)
+      {
+        max = userInput; 
+      }
+      counter++; 
+    }
+    System.out.println("The largest score is " + max);
+
+    // Problem 3 
+    String word = "calculator"; 
+    int index = 0; 
+    int counters = 0; 
+    while (index < word.length()); // get every letter loop 
+    {
+      counters++;
+      if (counters == 3)
+      {
+        counters = 0; 
+      }
+      else 
+      {
+      String currentLetter = word.substring(index, index+1); 
+      }
+      index++; 
+    }
+
+
   }
 }
